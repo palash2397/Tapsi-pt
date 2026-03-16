6b8999d6702e790f98736a2e6bc29f5967131763
palash2397 <palashpatil55@gmail.com>
2026-03-16 11:23:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import morgan from "morgan";
+
+import "dotenv/config.js";
+
+
+
+
+const app = express();
+const PORT = process.env.PORT;
+
+
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+
+app.use("/api/v1", express.static("public"));
+
+// import rootRouter from "./routes/root.routes.js";
+// app.use("/api/v1", rootRouter);
+
+// Routes
+app.get("/api", (req, res) => {
+  res.send("Welcome to College Nerd");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
