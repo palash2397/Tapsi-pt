56723022d21384c075593634bfe2dc516d0c0287
palash2397 <palashpatil55@gmail.com>
2026-03-16 11:28:38 +0530

    Add card routes and integrate with root router

---
+++ routes/card.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const cardRouter = Router();
+
+
+
+export default cardRouter;
--- routes/root.routes.js
+++ routes/root.routes.js
@@ -1,7 +1,13 @@
 import { Router } from "express";
 
+import cardRouter from "./card.routes.js";
+
+
 const rootRouter = Router();
+
+rootRouter.use("/card", cardRouter);
 
 
 
 export default rootRouter;
+
