66ff9ad0bc951c18c9db3edbb95328767cc574bb
palash2397 <palashpatil55@gmail.com>
2026-03-16 11:38:25 +0530

    Remove testing endpoint from card routes and controller

--- routes/card.routes.js
+++ routes/card.routes.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
-import { testing } from "../controllers/card/cardController.js";
+
 
 const cardRouter = Router();
 
-cardRouter.get("/testing", testing);
 
 export default cardRouter;
