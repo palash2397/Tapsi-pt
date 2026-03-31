4660d2d08643113b9c64f28493dd2c7fb3e34a7e
palash2397 <palashpatil55@gmail.com>
2026-03-31 10:43:57 +0530

    Remove checkout route from payment routes

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { createPayment, openCheckoutPage } from "../controllers/paymentController.js";
+import { createPayment } from "../controllers/paymentController.js";
 
 const paymentRouter = Router();
 
 paymentRouter.post("/sibs/create", createPayment);
-paymentRouter.get("/sibs/checkout", openCheckoutPage);
+
 
 export default paymentRouter;
